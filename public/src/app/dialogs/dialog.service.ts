d4ed7374885ca1edcf5d4b516e22cc98a9772bee
Ravi Chandran <chandrangreatfree@gmail.com>
2017-07-20 17:47:42 +0530

    Used HTTP Module to make a post call for addBoard Service

--- public/src/app/dialogs/dialog.service.ts
+++ public/src/app/dialogs/dialog.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class DialogService {
 
-  constructor() { }
+  private Url;
+  private headers;
 
-  addBoardName(name: String) {
+  constructor(private http: Http) {
+
+    this.Url = 'http://localhost:3000/boards';
+
+    this.headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+   }
+
+  addBoardName(name: String): Promise <any> {
     console.log('Name of the Board ' + name);
+    return this.http.post(this.Url, JSON.stringify({
+                name: name
+            }), { headers: this.headers } )
+            .toPromise()
+            .then(response => response.json().data)
+            .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise <any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 
 }
