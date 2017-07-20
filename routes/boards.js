5165c047ffd5449539adba54a45540f231c66979
Ravi Chandran <chandrangreatfree@gmail.com>
2017-07-20 14:49:08 +0530

    Removed another console.log() message

--- routes/boards.js
+++ routes/boards.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var controller = require('../controllers');
 
 router.post('/', function (req, res, next){
-    console.log(req.body);
     controller.board.add(req.body, (err, data) => {
         if(!err) res.send(data);
         else next(err);
