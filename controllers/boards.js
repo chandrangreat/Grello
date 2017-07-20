e6ffc0dd7602e057fb09794c226190f23e6769f2
Ravi Chandran <chandrangreatfree@gmail.com>
2017-07-20 14:41:18 +0530

    Using callbacks and passing error to the error handler middleware

--- controllers/boards.js
+++ controllers/boards.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var Board = mongoose.model('board'),
     board = {};
 
-board.add = (params) => {
+board.add = (params, cb) => {
     console.log('Inside controller param : '+ params);
     Board.create(params, function(err,id){
-        if(err) throw err;
-        else console.log('Created with id: '+id);
+        if(err) cb(err, null);
+        else cb(null, id);
     });
 }
 
--- routes/boards.js
+++ routes/boards.js
@@ -4,7 +4,10 @@ var controller = require('../controllers');
 
 router.post('/', function (req, res, next){
     console.log(req.body);
-    controller.board.add(req.body);
+    controller.board.add(req.body, (err, data) => {
+        if(!err) res.send(data);
+        else next(err);
+    });
 });
 
 module.exports = router;
