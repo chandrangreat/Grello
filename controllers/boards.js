d9a350093d54b95893992fc420745363eea02579
Ravi Chandran <chandrangreatfree@gmail.com>
2017-07-24 12:21:19 +0530

    API get method to retrieve a  Board using short Id

--- controllers/boards.js
+++ controllers/boards.js
@@ -5,7 +5,7 @@ var Board = mongoose.model('board'),
     board = {};
 
 board.add = (params, cb) => {
-    
+
     params["shortId"] = localShortId = shortid.generate();
     params["shortUrl"] = '/b/' + localShortId + '/' + params.name.toLowerCase().replace(' ', '-').replace('_', '');
 
@@ -13,6 +13,15 @@ board.add = (params, cb) => {
         if(err) cb(err, null);
         else cb(null, id);
     });
-}
+};
+
+board.get = (params, cb) => {
+    console.log(params);
+
+    Board.findOne({'shortId': params}, function(err, data){
+        if(err) cb(err, null);
+        else cb(null, data);
+    });
+};
 
 module.exports = board;
--- routes/boards.js
+++ routes/boards.js
@@ -9,4 +9,12 @@ router.post('/', function (req, res, next){
     });
 });
 
+router.get('/:id', function (req, res, next){
+    //console.log(req.params.id);
+    controller.board.get(req.params.id, (err, data) => {
+        if(!err) res.send({success: 'true', data: data});
+        else next(err);
+    });
+});
+
 module.exports = router;
